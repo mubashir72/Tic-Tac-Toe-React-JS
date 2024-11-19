535f6c4d14eaa3065f596ab13f3a34b6417a9742
mubashir72 <mmubashirmemon@gmail.com>
2024-11-19 13:19:43 +0500

    files uploaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+const initialBoard = ()=>Array(9).fill(null);
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [board, setBoard] = useState(initialBoard());
+  console.log(board)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='game'>
+      <div className="status">
+        Player X Turn 
+        <button> ResetGame</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <div className="board">
+        {board.map((_, index)=>{
+          return <button className="cell" key={index}>X</button>
+
+        })}
+        </div>
+    </div>
   )
 }
 
